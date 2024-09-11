f27db4670a6bfc5e9c3873749ecf4b43f9305bcc
Leonel Herrera <leonelherreradkno@gmail.com>
2024-09-11 14:45:19 -0300

    I've added the following validations
    - A validation to avoid a string without special or alphanumeric characters.
    - A match where the owner can only contain letters and spaces.

--- src/modules/checking-accounts/domain/dtos/create-current-account.dto.ts
+++ src/modules/checking-accounts/domain/dtos/create-current-account.dto.ts
@@ -1,11 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { TypesCurrentAccountEnum } from '@src/modules/checking-accounts/domain/enums/types-current-account.enum';
-import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateCurrentAccountDto {
   @ApiProperty()
   @IsNotEmpty()
-  @IsString()
+  @IsString({
+    message:
+      'The owner must be a string without special or alphanumeric characters.',
+  })
+  @Matches(/^[a-zA-Z\s]*$/, {
+    message: 'The owner can only contain letters and spaces.',
+  })
   owner: string;
 
   @ApiProperty()
